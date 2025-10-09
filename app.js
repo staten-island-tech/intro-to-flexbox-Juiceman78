c241e0ccec7107c5acd94279e967106ea3a4bfc4
Juiceman78 <vincentz78@nycstudents.net>
2025-10-09 14:01:11 -0400

    676767676767

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ const animalList = [
     name: "Rainbow-Bird",
     price: 3939.39,
     instock: true,
-    class: "Bird",
+    class: "Aves",
     img: "images/Rainbowbird.jpg",
     alt: "bird",
   },
@@ -93,10 +93,11 @@ const animalList = [
     instock: true,
     class: "Exotic",
     img: "images/imposter.jpg",
+    alt: "sus",
   },
   {
     name: "John-Pork",
-    price: 1000000.0,
+    price: 100000.0,
     instock: true,
     class: "Exotic",
     img: "images/Johnpork.jpg",
@@ -110,4 +111,75 @@ const animalList = [
     img: "images/Hairline.jpg",
     alt: "Holy-hair",
   },
+  {
+    name: "Grinch",
+    price: 399339.39,
+    instock: true,
+    class: "Exotic",
+    img: "images/Grinch.webp",
+    alt: "knee",
+  },
+  {
+    name: "Lion",
+    price: 7299.99,
+    instock: true,
+    class: "Mammal",
+    img: "images/lion.jpg",
+    alt: "the lion",
+  },
+  {
+    name: "Monster",
+    price: 57777.77,
+    instock: true,
+    class: "Exotic",
+    img: "images/kid.jpg",
+    alt: "mason",
+  },
+  {
+    name: "Eagle",
+    price: 9999.99,
+    instock: true,
+    class: "Aves",
+    img: "images/eagle.jpg",
+    alt: "amurica",
+  },
+  {
+    name: "Blobfish",
+    price: 4141.99,
+    instock: true,
+    class: "Fish",
+    img: "images/blob.jpg",
+    alt: "myat",
+  },
+  {
+    name: "Dog",
+    price: 933.99,
+    instock: true,
+    class: "Mammal",
+    img: "images/dog.jpg",
+    alt: "man's best friend",
+  },
 ];
+
+//create inject function
+
+function inject(item) {
+  //query the html where we inject the card
+  const container = document.querySelector(".container");
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `<h1>${item.name}</h1>, <h2>${item.price}</h2>,<img src="${item.img}" alt="${item.alt}">`
+  );
+}
+item.forEach((animal) => {});
+inject(animalList[0]);
+inject(animalList[1]);
+inject(animalList[2]);
+inject(animalList[3]);
+inject(animalList[4]);
+inject(animalList[5]);
+inject(animalList[6]);
+inject(animalList[7]);
+inject(animalList[8]);
+inject(animalList[9]);
+inject(animalList[10]);
